c02daa9b03e0ca1d5b0bed758694471204756290
asmita.dahatonde <asmita_dahatonde@persistent.com>
2015-12-24 15:57:10 +0530

    Web Job deployed

---
+++ WebJobCont/HelloWebJob/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace HelloWebJob
+{
+    using System;
+
+    using Microsoft.Azure.WebJobs;
+
+    /// <summary>
+    /// Web job class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Hello WOB JOBS !!");
+            Console.ReadLine();
+            var host = new JobHost();
+
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}

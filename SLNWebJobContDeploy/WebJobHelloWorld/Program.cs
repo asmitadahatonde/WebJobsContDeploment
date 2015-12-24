633a05e65ca63dcc5c3a4b2d9cf53d1cdcddef30
asmita.dahatonde <asmita_dahatonde@persistent.com>
2015-12-24 15:07:39 +0530

    Log Web jobs

---
+++ SLNWebJobContDeploy/WebJobHelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace WebJobHelloWorld
+{
+    using System;
+
+    /// <summary>
+    /// Hello world class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        /// <param name="args">Argument</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD !!");
+            Console.ReadLine();
+        }
+    }
+}
